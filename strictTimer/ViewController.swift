b704b34a9cc04043d11c01d9b1101bed715e3859
pyromani4 <sedonaarizona9@gmail.com>
2019-04-04 11:42:17 -0400

    Initial Commit

---
+++ strictTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  strictTimer
+//
+//  Created by RS on 2019/04/04.
+//  Copyright © 2019 RS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
